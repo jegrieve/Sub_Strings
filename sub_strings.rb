dfbeafb056d763da8ed9c2b0c3a95c6953f549d1
James Grieve <pocojames@hotmail.com>
2020-07-07 23:49:51 -0700

    removed comment

--- sub_strings.rb
+++ sub_strings.rb
@@ -3,7 +3,6 @@ def substrings(word, dictionary)
     sub_strings = Hash.new(0)
     k = word.length - 1
     while k > 0
-        # sub_strings[word[0..i]] += 1 if dictionary.include?(word[0..i])
         word.each_char.with_index do |letter, i|
         sub_strings[word[i..k]] += 1 if dictionary.include?(word[i..k])
         end
